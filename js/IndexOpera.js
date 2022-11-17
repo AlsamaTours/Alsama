1854d772ae8e8ad4f76d00fe0e8a0d9c783580bf
Bjuz <jeaustin.rdz@.com>
2022-11-16 22:54:29 -0600

    Changes

--- js/IndexOpera.js
+++ js/IndexOpera.js
@@ -96,7 +96,8 @@ async  function namePage(){
     let nombre = await URlPage(); //llamo para ver el URL
     const NombreService = nombre.split('#')[1]; //Uso lo que esta despues del # asi da una sensacion de php
     if(NombreService == "Exito"){
-        alert("gracias por la suscripcion, ahora tienes un 5% de descuento / thanks for subscribing, now you have a 5% discount");
+        toastr.info("Gracias por la suscripcion, ahora el correo "+ document.getElementById('email').value+" tienes un 5% de descuento \n Thanks for subscribing, now the email "+ document.getElementById('email').value +" have a 5% discount")
+        document.getElementById('email').value = "";
     }
 
 }
@@ -114,3 +115,27 @@ function SanJoseToursPDF(){
   function JacoToursPDF(){
     window.location.href= "Jaco%20One%20Day%20Tours.pdf";
   }
+
+const form  = document.getElementById('BtmForm');
+
+form.addEventListener('click', function(e) {
+    e.preventDefault();
+    const BodyEmail = "New posible cx, please make in touch with the cx as soon as possible"+'\n'+"Email: "+ document.getElementById('email').value
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Port: 2525,
+        Username : "automaticosemails@gmail.com",
+        Password : "827486E87EEDF8DC4246B3C76AC5CE9CBF18",
+        To : 'automaticosemails@gmail.com',
+        From : "Email: "+ document.getElementById('email').value,
+        Subject : "Email from a new possible cx",
+        Body : BodyEmail
+    }).then( function(){
+        toastr.info("Gracias por la suscripcion, ahora el correo "+ document.getElementById('email').value+" tienes un 5% de descuento \n Thanks for subscribing, now the email "+ document.getElementById('email').value +" have a 5% discount")
+        document.getElementById('email').value = "";
+
+    }
+    ).catch((error) => {
+        alert(error);
+      });
+});
